21fbed709fd1e460333ad413a08311bd34e47da1
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-03-09 21:30:50 +0100

    created olean backend

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+const MONGODB = process.env.MONGODB_URL;
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(MONGODB)
+  .then(() => {
+    console.log("Connect to MongooDB....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
